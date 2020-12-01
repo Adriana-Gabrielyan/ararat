ad4eeffb285dd697064ac2ca14b89a3d53a9ef7e
Narek Sargsyan <Nsarg7@gmail.com>
2020-12-01 11:18:01 +0400

    Added Login.page

--- src/App/App.js
+++ src/App/App.js
@@ -1,16 +1,5 @@
-import { memo } from "react";
-import { ReactComponent as ReactLogo } from "assets/logo.svg";
-const MemoLogo = memo(ReactLogo);
 const App = () => {
-    return (
-        <div className="app">
-            <main className="app__main">
-                <h1 className="u-text-align--center">Welcome to React</h1>
-                <h2>Find out whole beauty with right configuration</h2>
-                <MemoLogo className="app__logo" />
-            </main>
-        </div>
-    );
+    return <div className="app"></div>;
 };
 
 export default App;
